bee7ce99f149cc8c3c58ebdd1f64f3144665f892
Alvaro Aleman <alv2412@googlemail.com>
2015-10-22 23:15:08 +0200

    Add SeLinux compatibility

--- startscript.sh
+++ startscript.sh
@@ -9,14 +9,14 @@ shopt -s dotglob
 # Use config from package management if we dont have one already
 if [[ ! "$(ls -A $PERSISTENT_CONFIG)" ]]; then
   echo "Importing configuration from package management"
-  mv $TMP_CONFIG/* $PERSISTENT_CONFIG
+  mv -Z $TMP_CONFIG/* $PERSISTENT_CONFIG
   rm -rf $TMP_CONFIG
 fi
 
 # Use directroy structure from package management if we dont have any
 if [[ ! "$(ls -A $PERSISTENT_DATA)" ]]; then
   echo "Imorting user directory structure from package management"
-  mv $TMP_DATA/* $PERSISTENT_DATA
+  mv -Z $TMP_DATA/* $PERSISTENT_DATA
   rm -rf $TMP_DATA
   echo "Creating a ssh keypair"
   ssh-keygen -N '' -f $PERSISTENT_DATA/.ssh/id_rsa
